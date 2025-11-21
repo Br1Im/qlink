aead03424ba84dad160513ea736b0d965734f63d
BrIm <104125554+Br1Im@users.noreply.github.com>
2025-11-21 18:54:54 +0700

    Remove standalone output from next.config

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
   typescript: {
     // Отключаем проверку типов при сборке (для production)
     ignoreBuildErrors: true,
